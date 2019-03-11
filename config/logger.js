f288aae17590618973580c9d40daad9cd7b15127
chaouch salah eddine <chaouchsalaheddine97@hotmail.fr>
2019-03-11 16:12:42 +0100

    Configure the logger of the app

---
+++ config/logger.js
@@ -0,0 +1,29 @@
+'use strict';
+const { createLogger, format, transports } = require('winston');
+const fs = require('fs');
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const filename = path.join(logDir, `${process.env.LOG_DATE}.log`);
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.File({ filename })
+  ]
+});
+
+module.exports = logger;
