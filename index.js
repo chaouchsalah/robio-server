4378b8619d12226c79eb06392940da2b96000489
chaouch salah eddine <chaouchsalaheddine97@hotmail.fr>
2019-03-11 16:19:25 +0100

    Create a Cron job to save log data to database once a day

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+require('./config/checkEnvVariables');
+require('./web/server');
+require('./config/db');
+const cron = require('node-cron');
+const getTodayDate = require('./lib/todayDate');
+const logger = require('./config/logger');
+
+// Will run at 12:00 PM everyday
+cron.schedule('0 00 * * *', function() {
+    process.env.LOG_DATE = getTodayDate();
+    // TODO: Save log of previous day into DB
+});
+
+// Log all uncaught errors
+process.on('uncaughtException', function(err) {
+    logger.error(err);
+});
+
+process.on('unhandledRejection', function(err) {
+    logger.error(err);
+});
