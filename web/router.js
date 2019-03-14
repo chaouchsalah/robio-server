c4c2b34d3e554c91568b5ae43b04b0dd25646bfd
chaouch salah eddine <chaouchsalaheddine97@hotmail.fr>
2019-03-14 09:33:53 +0100

    Configure HTTPS

--- web/router.js
+++ web/router.js
@@ -1,5 +1,2 @@
 module.exports = (app) => {
-    app.get('/', function(req, res) {
-        res.json({ message:'message'});
-    });
 };
--- web/server.js
+++ web/server.js
@@ -4,12 +4,22 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const fs = require('fs');
 const mainRouter = require('./router');
 const userRouter = require('../components/user/config/router');
 const authRouter = require('../components/authentication/config/router');
+const sekhraRouter = require('../components/sekhras/config/router');
 const logger = require('../config/logger');
 
 const app = express();
+
+const options = {
+    key: fs.readFileSync('./certs/server.key'),
+    cert: fs.readFileSync('./certs/server.crt')
+  };
+
+const server = require('https').Server(options, app);
+const io = require('socket.io')(server);
 
 app.use(cors());
 app.use(helmet());
@@ -22,11 +32,19 @@ app.use(passport.initialize());
 mainRouter(app);
 userRouter(app);
 authRouter(app);
+sekhraRouter(app);
 
 const port = process.env.PORT || 6200;
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
+});
+
+io.on('connection', socket => {
+    socket.on('position', (data) => {
+        console.log(data);
+        io.sockets.emit('locateUser', data);
+    });
 });
 
 app.use((err, request, response, next) => {
